3e272e0a12a74c00c0033620a261faf0eeaafb22
Sam Gruse <sgruse89@gmail.com>
2016-04-11 14:33:20 -0700

    Save Edit and cancel buttons working correctly

--- src/todos/todos.js
+++ src/todos/todos.js
@@ -1,16 +1,54 @@
 export default function($scope) {
+
+  let params = {
+    createHasInput: false
+  }
+
   $scope.todos = [
     {
       task: 'do dishes',
-      isCompleted: false
+      isCompleted: false,
+      isEditing: false
     },
     {
       taks: 'walk the dog',
-      isCopmleted: true
+      isCopmleted: true,
+      isEditing: false
     }
   ];
 
   $scope.onCompletedClick = (todo) => {
     todo.isCompleted = !todo.isCopmleted;
   };
+
+  $scope.onEditClick = (todo) => {
+    todo.isEditing = true;
+    todo.updateTask = todo.task;
+  }
+
+  $scope.onCancelClick = (todo) => {
+    todo.isEditing = false;
+  }
+
+  $scope.updateTask = (todo) => {
+    todo.task = todo.updatedTask;
+    todo.isEditing = false;
+  }
+
+  $scope.createTask = () => {
+    params.createHasInput = false;
+    $scope.createTaskInput = '';
+  };
+
+  $scope.$watch('createTaskInput', (val) => {
+    if (!val && params.createHasInput) {
+      $scope.todos.pop();
+      params.createHasInput = false;
+    } else if (val && !params.createHasInput) {
+      $scope.todos.push({ task: val, isCopmleted: false});
+      params.createHasInput = true;
+    } else if (val && params.createHasInput) {
+      $scope.todos[$scope.todos.length - 1].task = val;
+    }
+  })
 }
